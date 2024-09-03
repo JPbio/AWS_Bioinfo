43be87b5bee17e754d1ee0347a4c0b0aa4e7ba46
João Paulo Almeida <64036432+JPbio@users.noreply.github.com>
2024-09-03 01:14:02 -0300

    Update SRA_to_trimfasta_SE.sh

--- SRA_to_trimfasta_SE.sh
+++ SRA_to_trimfasta_SE.sh
@@ -71,7 +71,7 @@ if [ "$stop_instance" = true ]; then
   
   ### Get the region of the EC2 instance
   #region=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/\(.*\)[a-z]/\1/')  #too many conditions to work
-  region=$(ec2-metadata -z | cut -f2 -d ":" | sed -e 's/^ //g' | sed 's/b//g') # needs amazon-ec2-utils installed
+  region=$(ec2-metadata -z | cut -f2 -d ":" | sed -e 's/^ //g' | sed -e 's/[a-z]$//g') # needs amazon-ec2-utils installed
  
   ### stopping the instace
   sudo aws ec2 stop-instances --instance-ids "$instance_id" --region "$region" #need to fix the need of sudo
